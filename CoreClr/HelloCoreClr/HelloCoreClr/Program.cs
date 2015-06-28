f7e03a1bced34a861f59625507c85d2a5cbb6e72
fancy Development - Daniel Murrmann <daniel.murrmann@fancy-development.net>
2015-06-28 16:45:13 +0200

    Added sample for hosting .NET Core with native bootstrapper

---
+++ CoreClr/HelloCoreClr/HelloCoreClr/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCoreClr
+{
+    public class Program
+    {
+        /// <summary>
+        /// Entry point of the application
+        /// </summary>
+        public static void Main()
+        {
+            Console.WriteLine("Hello Core Clr");
+            Console.ReadLine();
+        }
+    }
+}
